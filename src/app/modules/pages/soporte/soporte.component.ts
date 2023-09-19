f6339d17fcaf8c6e6808868297135991e5463725
wlopez20 <wlopezluna@gmail.com>
2023-09-19 11:35:50 -0500

    Update ts

--- src/app/modules/pages/soporte/soporte.component.ts
+++ src/app/modules/pages/soporte/soporte.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {HelpdeskService} from "../../services/helpdesk/helpdesk.service";
-import {TipoAtencion} from "../../../model/helpdesk/TipoAtencion";
-import {Requerimientos} from "../../../model/helpdesk/Requerimientos";
 
 @Component({
   selector: 'app-soporte',
@@ -10,41 +7,9 @@ import {Requerimientos} from "../../../model/helpdesk/Requerimientos";
 })
 export class SoporteComponent implements OnInit {
 
-  tipoAtencion: TipoAtencion[] = [];
-  requerimientos: Requerimientos[] = [];
-
-  constructor(private helpdeskService: HelpdeskService) {
-    this.listTipoAtencion();
-    this.listRequerimientos();
-    this.listTipoAtencion2()
-  }
+  constructor() { }
 
   ngOnInit(): void {
 
-  }
-
-  listTipoAtencion2(): void{
-    this.helpdeskService.listTipoAtencion2().subscribe({
-      next:(resp) => {
-        console.log(resp)
-      }
-    })
-  }
-
-  listTipoAtencion(): void{
-    this.helpdeskService.listTipoAtencion().subscribe({
-      next:(resp) => {
-        this.tipoAtencion = resp;
-        console.log(this.tipoAtencion)
-      }
-    })
-  }
-  listRequerimientos(): void{
-    this.helpdeskService.listRequerimientos().subscribe({
-      next:(resp) => {
-        this.requerimientos = resp;
-        console.log(this.requerimientos)
-      }
-    })
   }
 }
